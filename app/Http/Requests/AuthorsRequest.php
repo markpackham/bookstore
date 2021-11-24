50425d44a653c30d18b769cd48e8685eea7773df
mark <markpackham1@gmail.com>
2021-11-24 13:26:24 +0000

    add a validation check for author put requests
    https://youtu.be/BnBOigp2mWw?t=241

--- app/Http/Requests/AuthorsRequest.php
+++ app/Http/Requests/AuthorsRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateAuthorRequest extends FormRequest
+class AuthorsRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class UpdateAuthorRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:authors|max:255'
+            'name' => 'required|unique:authors\max:255'
         ];
     }
 }
